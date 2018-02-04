14a7904a963fcf0a1ef3190505b87ff95f3acba8
Nicholas <nicholas.rosqvist.nunes@gmail.com>
2018-02-04 21:26:25 +0100

    Delete pn.c

--- bikka/phase1/stanlib/commands_src/pn/pn.c
+++
@@ -1,9 +0,0 @@
-#include "pn.h"
-
-/* Simpel patch note(pn) screen*/
-
-void pn(){
-    Say("## The most reason patch note ##\n\n");
-    Say("# Patch 1\n");
-    Say("* First phase of Bikka was released");
-}
